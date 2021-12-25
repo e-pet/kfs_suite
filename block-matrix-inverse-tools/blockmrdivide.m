aff7390c6a62cd4e92466b3d8074b825ba38d3f4
e-pet <1774207+e-pet@users.noreply.github.com>
2021-12-25 17:45:06 +0100

    Delete block-matrix-inverse-tools directory

--- block-matrix-inverse-tools/blockmrdivide.m
+++
@@ -1,8 +0,0 @@
-function R = blockmrdivide(M, subsizes, b, varargin)
-% BLOCKMRDIVIDE implement matrix right-divide (see @mrdivide), as in the solution to x*M = b
-% implemented as x = b / M. Here, x = blockmldivide(M, subsizes, b) where 'subsizes' is defined as
-% in @blockinv. See @blockinv and @blockmldivide for further details.
-
-R = b * blockinv(M, subsizes, varargin{:});
-
-end
